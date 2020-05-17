d885cd1fc90269a22dba8ce4689c4aa0915a8a50
Syntaxx007 <ogundipeibukun51@gmail.com>
2020-05-17 22:57:57 +1200

    Add files via upload

---
+++ Day 16/main.js
@@ -0,0 +1,6 @@
+// A Program which tells the number of days in a month.
+
+// Get the Modal 
+var modal = document.getElementById("modalPop");
+// Get the button that opens the modal
+document.getElementById("popBtn").addEventListener("click",function(){modal.style.display="block";},true);
