3917bb03deeea48311f35047d54c7c35a059ee5c
Syntaxx007 <ogundipeibukun51@gmail.com>
2020-05-18 17:37:03 +0100

    Delete main.js

--- Day 17/main.js
+++
@@ -1,6 +0,0 @@
-// A Program which tells the number of days in a month.
-
-// Get the Modal 
-var modal = document.getElementById("modalPop");
-// Get the button that opens the modal
-document.getElementById("popBtn").addEventListener("click",function(){modal.style.display="block";},true);
