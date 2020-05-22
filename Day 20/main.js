7f33ed5260fb2c102934580f4871822d353f1b30
Syntaxx007 <ogundipeibukun51@gmail.com>
2020-05-22 19:45:12 +1200

    Add files via upload

---
+++ Day 20/main.js
@@ -0,0 +1,26 @@
+
+// Change the body background color to red
+document.getElementById("button1").addEventListener("click",
+    function(){
+        document.body.style.backgroundColor= "red";
+})
+// Change the body background color to blue
+document.getElementById("button2").addEventListener("click",
+    function(){
+        document.body.style.backgroundColor= "blue";
+})
+
+// Change the body background color to yellow
+document.getElementById("button3").addEventListener("click",
+    function(){
+        document.body.style.backgroundColor= "yellow";
+})
+
+ // Change the body background color to white
+document.getElementById("button4").addEventListener("click",
+function(){
+    document.body.style.backgroundColor= "white";
+})
+    
+
+
